52e6d0da59479617fd9b6170a6e55074374182bc
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-16 09:01:49 +0530

    validate phone number

--- UserRegistrtion/UserRegistrtion/UcerRegister.cs
+++ UserRegistrtion/UserRegistrtion/UcerRegister.cs
@@ -12,6 +12,7 @@ namespace UserRegistration
         public string regex_FirstName = "^[A-Z]{1}[a-z]{2,8}$";
         public string regex_LastName = "^[A-Z]{1}[a-z]{2,8}$";
         public string regex_EmailID = "^[a-z0-9]{1,10}[.]*[a-z0-9]{1,10}[@]{1}[a-z]{1,5}[.]*[a-z]{1,3}$";
+        public string regex_MobileNO = "^[+]*[9]{1}[1]{1}[' ']*[0-9]{10}$";
 
         public bool validateFirstName(string firstname)
         {
@@ -24,6 +25,10 @@ namespace UserRegistration
         public bool validateEmailID(string email)
         {
             return Regex.IsMatch(email, regex_EmailID);
+        }
+        public bool validateMobileNumber(string mobilenum)
+        {
+            return Regex.IsMatch(mobilenum, regex_MobileNO);
         }
     }
 }
