10a520c4271d3f3adf1b521c6b3b1074e9a6358e
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-16 09:26:17 +0530

    validate password rule

--- UserRegistrtion/UserRegistrtion/UcerRegister.cs
+++ UserRegistrtion/UserRegistrtion/UcerRegister.cs
@@ -13,7 +13,7 @@ namespace UserRegistration
         public string regex_LastName = "^[A-Z]{1}[a-z]{2,8}$";
         public string regex_EmailID = "^[a-z0-9]{1,10}[.]*[a-z0-9]{1,10}[@]{1}[a-z]{1,5}[.]*[a-z]{1,3}$";
         public string regex_MobileNO = "^[+]*[9]{1}[1]{1}[' ']*[0-9]{10}$";
-        public string regex_Password = "^[A-Z]{1}[a-z]{7}$";
+        public string regex_Password = "^[A-z]{1}[a-z0-9]{7}$";
 
         public bool validateFirstName(string firstname)
         {
