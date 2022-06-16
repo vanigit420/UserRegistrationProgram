dcdf4aa87a566369d556de1e8a296dad504d8811
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-16 08:42:56 +0530

    validating fist name

---
+++ UserRegistrtion/UserRegistrtion/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the User Registration Program");
+
+            UserRegister userRegister = new UserRegister();
+            Console.WriteLine("Enter the First Name: ");
+            string firstname = Console.ReadLine();
+
+            if (userRegister.validateFirstName(firstname))
+            {
+                Console.WriteLine("Pattern Match");
+            }
+            else
+            {
+                Console.WriteLine("Pattern not Match");
+            }
+        }
+    }
+}
---
+++ UserRegistrtion/UserRegistrtion/UcerRegister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class UserRegister
+    {
+        public string regex_FirstName = "^[A-Z]{1}[a-z]{2,8}$";
+
+        public bool validateFirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, regex_FirstName);
+        }
+    }
+}
